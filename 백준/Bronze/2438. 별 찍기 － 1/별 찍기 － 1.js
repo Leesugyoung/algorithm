a23563f2ecb0c14f1fc980d461463049e240350a
LEE SU-GYEONG <tnrud4685@naver.com>
2023-07-12 22:42:14 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 140 ms, Memory: 9480 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.js
@@ -0,0 +1,12 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().split(' ');
+
+let n = Number(input[0]);
+let result ="";
+
+for (let i = 0; i < n; i++){
+    for (let j=0; j <= i; j++) {
+        result += "*";
+    }
+    result += "\n";
+}
+console.log(result);
